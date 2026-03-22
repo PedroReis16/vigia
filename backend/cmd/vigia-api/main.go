87b1abb79e735fe9149cde38bd50966dd8bf1863
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-03-22 15:19:53 -0300

    feat: Separação do módulo GO para o projeto console e API + Makefile de inicialização

--- backend/cmd/vigia-api/main.go
+++ backend/cmd/vigia-api/main.go
@@ -1,15 +1,15 @@
-package cmd
+package main
 
 import (
 	"github.com/gin-gonic/gin"
 )
 
-func Execute() error {
+func main(){
 	r := gin.Default()
 	r.GET("/gerenciamento-dispositivos/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Bem vindo ao Vigia! Sua plataforma para o cuidado de quem você mais ama",
 		})
 	})
-	return r.Run(":8000")
+	r.Run(":8000")
 }

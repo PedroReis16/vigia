08a8ea110434466d25454f45f11834a29e8cdc3b
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-03-22 14:24:17 -0300

    fix: Vulnerabilidade inicial código go

--- backend/cmd/root.go
+++ backend/cmd/root.go
@@ -4,12 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Execute() {
+func Execute() error {
 	r := gin.Default()
 	r.GET("/gerenciamento-dispositivos/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Bem vindo ao Vigia! Sua plataforma para o cuidado de quem você mais ama",
 		})
 	})
-	r.Run(":8000")
+	return r.Run(":8000")
 }

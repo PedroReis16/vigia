823b7c7e0b88692d992a72642a63de32a6862997
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-03-30 19:51:52 -0300

    feat: retângulo para delimitação de uma região do vídeo

--- fall-detection/src/main.py
+++ fall-detection/src/main.py
@@ -10,6 +10,17 @@ while True:
     if not ret:
         break
 
+    height, width = frame.shape[:2]
+    
+    x1 = int(width * 0.05) # 5% do frame para cada lado (esquerda)
+    x2 = int(width * 0.95) # 5% do frame para cada lado (direita)
+    y1 = int(height * 0.05) # 5% do frame para cada lado (topo)
+    y2 = int(height * 0.95) # 5% do frame para cada lado (base)
+
+    roi = frame[y1:y2, x1:x2]
+
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
     # 3. Exibir o frame
     cv2.imshow('Webcam', frame)
 

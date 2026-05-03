cf70cf560cda12bc9178e82b280544eb2dc79ff5
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-05-03 18:49:43 -0300

    feat: Intepretação da janela deslizante com o classificador binario

--- fall-detection/src/app/core/action_classifier.py
+++ fall-detection/src/app/core/action_classifier.py
@@ -1,7 +1,7 @@
 import datetime
 import queue
 
-
+from app.core.fall_classifier import FallClassifier
 
 def _send_notification() -> None:
     """Envia notificação para o usuário"""
@@ -11,18 +11,24 @@ def _send_notification() -> None:
 
 def run_classifier(buffer_queue: queue.Queue) -> None:
     """Consome janelas prontas de captura, iniciando o processo de classificação"""
-    
+
+    fall_classifier = FallClassifier()
 
     # gru_model = Model() -> Carrega o modelo GRU
 
-    while True:
-        try:
-            pid, window = buffer_queue.get(timeout=1.0)
+    try:
 
-            # Envia para o modelo
-            results = []
-            print(f"Janela recebida para o ID '{pid}'")
-            _send_notification()
 
-        except queue.Empty:
-            continue # fila vazia, espera próxima janela
+        while True:
+            try:
+                pid, window = buffer_queue.get(timeout=1.0)
+
+                pred = fall_classifier.predict(window[0])
+                if pred is not None:
+                    print(f"Predição para o ID '{pid}': {pred['label']} com probabilidade {pred['prob_deitado']}")
+                    _send_notification()
+            except queue.Empty:
+                continue # fila vazia, espera próxima janela
+
+    except KeyboardInterrupt:
+        pass

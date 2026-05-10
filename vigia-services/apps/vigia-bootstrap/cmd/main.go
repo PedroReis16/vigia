affa86ea8b6913fe2a0f219a2d420951342d53e6
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-05-09 21:20:10 -0300

    chore: Remoção da lógica de bootstrap como serviço de constante

--- vigia-services/apps/vigia-bootstrap/cmd/main.go
+++ vigia-services/apps/vigia-bootstrap/cmd/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
-	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	if err := Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+	var rootCommand = &cobra.Command{}
+
+	var projectName, projectPath string
+
+	var cmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create boilerplate for a new project",
+		Run: func(cmd *cobra.Command, args []string) {
+			// validations
+			if projectName == "" {
+				fmt.Println("You must supply a project name.")
+				return
+			}
+			if projectPath == "" {
+				fmt.Println("You must supply a project path.")
+				return
+			}
+			fmt.Println("Creating project...")
+		},
 	}
+
+	cmd.Flags().StringVarP(&projectName, "name", "n", "", "Name of the project")
+	cmd.Flags().StringVarP(&projectPath, "path", "p", "", "Path where the project will be created")
+
+	rootCommand.AddCommand(cmd)
+	rootCommand.Execute()
 }

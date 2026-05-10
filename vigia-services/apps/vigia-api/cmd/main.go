7d6af3472c08083c797a883d444b0728189d52d9
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-05-09 21:48:11 -0300

    feat: Introdução do swagger da API prinicipal

--- vigia-services/apps/vigia-api/cmd/main.go
+++ vigia-services/apps/vigia-api/cmd/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	_ "github.com/PedroReis16/vigia/vigia-services/apps/vigia-api/docs"
+
 	"github.com/PedroReis16/vigia/vigia-services/apps/vigia-api/config"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
 )
 
+// @title           Vigia API
+// @version         1.0
+// @description     API do Vigia Services.
+// @host            localhost:8000
+// @BasePath        /
 func main() {
 	app := fx.New(
 		fx.Provide(

f3b7297c225d7dc5feeb2bd58c5c1c5d7b1e730d
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-05-09 21:44:41 -0300

    feat: Declaração base das rotas relacionadas ao dispositivo

--- vigia-services/apps/vigia-api/internal/http/handlers/devices_handler.go
+++ vigia-services/apps/vigia-api/internal/http/handlers/devices_handler.go
@@ -1,8 +1,35 @@
 package handlers
 
-type DevicesHandler struct {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DevicesHandler struct{}
 
 func NewDevicesHandler() *DevicesHandler {
 	return &DevicesHandler{}
 }
 
+func (h *DevicesHandler) RegisterUserDevice(c *gin.Context){
+	// Vincula um dispositivo a um usuário
+
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+}
+
+func (h *DevicesHandler) GetDevices(c *gin.Context) {
+
+	// Retorna os dispositivos cadastrados para o usuário
+
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+}
+
+func (h *DevicesHandler) DownloadVigiaVersion(c *gin.Context) {
+
+	// Baixa a versão do Vigia para o dispositivo
+
+    // Se for passado o parâmetro de uma versão específica, será retornado a versão, se não a última versão disponível
+
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+}

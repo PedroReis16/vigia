252b53e06f16127450349147b6b8b8d249e998af
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-08-01 15:58:53 -0300

    feat: Persistência do userId a partir do token JWT

--- vigia_ui/lib/data/services/token_storage_service.dart
+++ vigia_ui/lib/data/services/token_storage_service.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class TokenStorageService {
   static const String _accessTokenKey = 'access_token';
   static const String _refreshTokenKey = 'refresh_token';
+  static const String _userIdKey = 'user_id';
 
   final _storage = FlutterSecureStorage();
 
   Future<void> saveUserTokens(String accessToken, String refreshToken) async {
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(accessToken);
+
+    final String userId = decodedToken['sub'] as String;
+
+    await _storage.write(key: _userIdKey, value: userId);
+
     await _storage.write(key: _accessTokenKey, value: accessToken);
     await _storage.write(key: _refreshTokenKey, value: refreshToken);
   }
@@ -22,5 +30,13 @@ class TokenStorageService {
 
   Future<String?> getRefreshToken() async {
     return await _storage.read(key: _refreshTokenKey);
+  }
+
+  Future<String> getUserId() async {
+    final String? userId = await _storage.read(key: _userIdKey);
+    if (userId == null) {
+      throw Exception('User ID not found');
+    }
+    return userId;
   }
 }

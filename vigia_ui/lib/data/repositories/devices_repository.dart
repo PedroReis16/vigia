e469dae44b6d265fbe568c398aa1d35059779348
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-08-01 15:58:53 -0300

    feat: Atualização de dispositivo e consulta de usuários no repositório

--- vigia_ui/lib/data/repositories/devices_repository.dart
+++ vigia_ui/lib/data/repositories/devices_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:vigia_ui/domain/DTOs/device.dart';
+import 'package:vigia_ui/domain/DTOs/user.dart';
+import 'package:vigia_ui/domain/enums/device_rooms.dart';
 import 'package:vigia_ui/domain/exceptions/unauthroized_exception.dart';
 
 class DevicesRepository {
@@ -27,6 +29,54 @@ class DevicesRepository {
       throw Exception('Failed to get devices');
     } catch (e) {
       throw Exception('Failed to get devices');
+    }
+  }
+
+  Future<List<DeviceUser>> getDeviceUsers(String deviceId) async {
+    try {
+      final response = await dio.get("$_devicesEndpoint/$deviceId/users");
+
+      List<DeviceUser> users = (response.data as List)
+          .map((user) => DeviceUser.fromJson(user))
+          .toList();
+
+      return users;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw UnauthorizedException(
+          "Permissões insuficientes para acessar os dispositivos.",
+        );
+      }
+      throw Exception('Failed to get device users');
+    } catch (e) {
+      throw Exception('Failed to get device users');
+    }
+  }
+
+  Future<void> updateDevice(
+    String deviceId, {
+    String? nickname,
+    DeviceRooms? room,
+    bool? isClipsEnabled,
+  }) async {
+    try {
+      await dio.put(
+        "$_devicesEndpoint/$deviceId",
+        data: {
+          "nickname": nickname,
+          "room": room?.toApiString(),
+          "isClipsEnabled": isClipsEnabled,
+        },
+      );
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw UnauthorizedException(
+          "Permissões insuficientes para acessar os dispositivos.",
+        );
+      }
+      throw Exception('Failed to update device');
+    } catch (e) {
+      throw Exception('Failed to update device');
     }
   }
 }

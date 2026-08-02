63f423612c49c89e484656029744f2998e9a0baf
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2026-08-02 20:40:50 -0300

    fix: Carregamento repetitivo de thumbnails

--- vigia-api/Libraries/Vigia.Cache/Services/IRedisCacheService.cs
+++ vigia-api/Libraries/Vigia.Cache/Services/IRedisCacheService.cs
@@ -3,6 +3,7 @@ namespace Vigia.Cache.Services;
 public interface IRedisCacheService
 {
     T? Get<T>(string key);
+    bool Exists(string key);
     void Add<T>(string key, T value);
     void Add<T>(string key, T value, TimeSpan expiration);
     void Remove(string key);

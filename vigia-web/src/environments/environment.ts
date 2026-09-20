b3431f004668b0138a248704da9cd6d15e79ced0
Pedro Reis <pedro_henrique_dsr@hotmail.com>
2026-09-20 16:35:59 -0300

    feat: Adição do setup do firebase para execução das notificações

--- vigia-web/src/environments/environment.ts
+++ vigia-web/src/environments/environment.ts
@@ -11,14 +11,14 @@ export const environment = {
   apiUrl: 'http://localhost:81/vigia',
   streamBaseUrl: 'http://localhost:81',
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "",
+    apiKey: 'AIzaSyDLv9uHA6xFbRRnwjRmzWaCxadX-v62DvI',
+    authDomain: 'vigia-fall-detection.firebaseapp.com',
+    projectId: 'vigia-fall-detection',
+    storageBucket: 'vigia-fall-detection.firebasestorage.app',
+    messagingSenderId: '241599132191',
+    appId: '1:241599132191:web:08154d96d34f15cf18db2a',
+    measurementId: 'G-7DG1Q5EQP8',
     // Firebase Console → Cloud Messaging → Web Push certificates → Key pair (pública)
-    vapidKey: "",
+    vapidKey: 'BAU11UKsG0rBMlbkj5YQa4D7CmjLT6r5Zda1J7K90SoEQjZ_Ig9TLrR7PdzQqFUtD5vJqwzJeAcmW8aKt3sJe4E',
   } satisfies FirebaseEnvironmentConfig,
 };
